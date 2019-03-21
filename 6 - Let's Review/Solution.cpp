9a44fb01bf2c419a1d3eb65bda58f869afc19698
Arnav Saha <44087308+sahaarnav3@users.noreply.github.com>
2019-03-21 09:57:00 +0530

    Update solution.cpp

--- 6 - Let's Review/Solution.cpp
+++ 6 - Let's Review/Solution.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
 using namespace std;
+string c[10];
 
 int main() {
     int N;
     cin >> N;
 
-    for (int i = 0; i < N; i++) {
-        string str;
-        cin >> str;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> c[i];
+    }
+    
+    for (int i = 0; i < N; i++)
+    { string str = c[i];
 
         for (int j = 0; j < str.length(); j++) {
             if (j % 2 == 0) cout << str[j];
@@ -22,6 +27,5 @@ int main() {
 
         cout << endl;
     }
-
     return 0;
 }
